e19bc932814b61f2cdc82cf66aefb9fd1306ef3a
Artur <example@example.com>
2023-09-21 17:38:40 +0200

    Add endpoints

---
+++ src/Tivix.FamilyBudget.Server.API/Endpoints/Budgets/BudgetPost.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Tivix.FamilyBudget.Server.Core.Budgets.Commands;
+
+namespace Tivix.FamilyBudget.Server.API.Endpoints.Budgets;
+
+public class BudgetPost : EndpointBaseAsync.WithRequest<CreateBudgetCommand>.WithActionResult<Guid>
+{
+    public override Task<ActionResult<Guid>> HandleAsync(CreateBudgetCommand request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Tivix.FamilyBudget.Server.Core/Budgets/Commands/CreateBudgetCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Tivix.FamilyBudget.Server.Core.Budgets.Commands
+{
+    internal class CreateBudgetCommandValidator : AbstractValidator<CreateBudgetCommand>
+    {
+        public CreateBudgetCommandValidator()
+        {
+            RuleFor(p=>p.OwnerId).NotEmpty();
+
+            RuleFor(p=>p.Name).NotEmpty();
+        }
+    }
+}
