b26c4d9ed76b877ab7168cfdb710fbf52696caac
Artur <example@example.com>
2023-09-22 00:37:15 +0200

    User migration

--- src/Tivix.FamilyBudget.Server.Infrastructure/Migrations/20230921223701_User.Designer.cs
+++ src/Tivix.FamilyBudget.Server.Infrastructure/Migrations/20230921223701_User.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tivix.FamilyBudget.Server.Infrastructure.DAL;
@@ -11,9 +12,11 @@ using Tivix.FamilyBudget.Server.Infrastructure.DAL;
 namespace Tivix.FamilyBudget.Server.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230921223701_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
