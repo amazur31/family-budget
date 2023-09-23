ce65566a06d6841fc4d6a1f94478efaa5be2bf9a
Artur <example@example.com>
2023-09-23 15:50:57 +0200

    DeleteCategories

---
+++ src/Tivix.FamilyBudget.Server.Core/Categories/Commands/DeleteCategoryCommand/DeleteCategoryCommand.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Tivix.FamilyBudget.Server.Core.Users.Providers;
+using Tivix.FamilyBudget.Server.Core.Users.Validators;
+using Tivix.FamilyBudget.Server.Infrastructure.DAL;
+
+namespace Tivix.FamilyBudget.Server.Core.Categories.Commands.UpdateCategoryCommand;
+public record DeleteCategoryCommand(Guid Id) : IRequest;
+
+internal class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+{
+    readonly ApplicationContext _context;
+    public DeleteCategoryCommandHandler(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _context.Categories.FirstAsync(c => c.Id == request.Id, cancellationToken: cancellationToken);
+        _context.Categories.Remove(category);
+        _ = _context.SaveChangesAsync(cancellationToken);
+    }
+}
+
+internal class DeleteCategoryCommandHandlerValidator : AbstractValidator<DeleteCategoryCommand>
+{
+    public DeleteCategoryCommandHandlerValidator(IUserProvider provider, ApplicationContext applicationContext)
+    {
+        RuleFor(p => p.Id).SetValidator(new UserCategoryValidator(provider, applicationContext));
+    }
+}
